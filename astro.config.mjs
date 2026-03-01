d7eafb329a65951578a326ab4b32564334a942f6
Stuart <stuartaharris@gmail.com>
2026-02-28 18:19:21 -0600

    chore: Adjust site config for debugging base path

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://SAHarris.github.io',
+  site: 'https://SAHarris.github.io/skaldy',
   base: '/skaldy',
 });
